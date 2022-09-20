8e5ec478b6c784a7b78fd837419f37ff43ca915e
Min Chang <min.chang@Min-Chang-MacBook-Pro.local>
2022-09-20 14:31:35 -0500

    Add initial files

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const container = document.getElementById('root') //accessing HTML in public/index.html for an element with an id of root
+// the container variable above will eventually contain all react components. (location where our react app will be rendered)
+const root = ReactDOM.createRoot(container)
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+// root.render will renders (runs) the react componnets
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
