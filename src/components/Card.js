fed0148cd819b4bb40977130e64f17f3446f8757
Min Chang <min.chang@Min-Chang-MacBook-Pro.local>
2022-09-23 13:49:02 -0500

    Add card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,3 +7,20 @@ function Card(props) {
         </React.Fragment>
     )
 }
+
+Card.propTypes = {
+    cardNumber: PropTypes.string,
+    CVV: PropTypes.string,
+    expY: PropTypes.string,
+    expM: PropTypes.string,
+    fullName: PropTypes.string,
+    addressl1: PropTypes.string,
+    postal: PropTypes.string,
+    city: PropTypes.string,
+    district: PropTypes.string,
+    countryCode: PropTypes.string,
+    phone: PropTypes.string,
+    email: PropTypes.string
+}
+
+export default Card;
