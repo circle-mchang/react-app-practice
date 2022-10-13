85b403b09594b77e1b9c519e0cdc39ad175bafff
Min Chang <min.chang@circle.com>
2022-10-12 19:37:15 -0500

    Add card details component

--- src/components/CardControl.js
+++ src/components/CardControl.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NewCardForm from "./NewCardForm";
 import CardList from "./CardList"
+import CardDetail from "./CardDetail"
 
 class CardControl extends React.Component {
     constructor(props){
@@ -8,7 +9,8 @@ class CardControl extends React.Component {
         this.state = {
             formVisibleOnPage: false,
             // move mainCardList into state
-            mainCardList: [] 
+            mainCardList: [], 
+            selectedCard: null
         }
     }
 
@@ -22,17 +24,34 @@ class CardControl extends React.Component {
         const newMainCardList = this.state.mainCardList.concat(newCard);
         this.setState({mainCardList: newMainCardList, formVisibleOnPage: false});
     }
+    // filtering out the array to have the id that matches the UUID since there is only one unique UUID for every card
+    // then set the state to show only the selectedCard within the array and specifying the index of the array to 0 since it will only have one card in the array
+    handleChangingSelectedCard = (id) => {
+        const newDetail = this.state.mainCardList.filter(card => card.id === id)[0];
+        this.setState({selectedCard: selectedCard});
+    }
 
     render(){
         let currentlyVisibleState = null;
         let buttonText = null;
-        if(this.state.formVisibleOnPage) {
-            // passing handleAddingNewCardToList method down to the NewCardForm component as a prop
+        // if(this.state.formVisibleOnPage) {
+        //     // passing handleAddingNewCardToList method down to the NewCardForm component as a prop
+        //     currentlyVisibleState = <NewCardForm onNewCardCreation={this.handleAddingNewCardToList}/>
+        //     buttonText = "Return to Card List"
+        // } else {
+        //     // passing mainCardList state as a prop to cardList <CardList cardList={this.state.mainCardList}/>
+        //     currentlyVisibleState = <CardList cardList={this.state.mainCardList}/>
+        //     buttonText = "Link New Card"
+        // }
+        if (this.state.selectedCard != null) {
+            currentlyVisibleState = <CardDetail card = {this.state.selectedCard} />
+            buttonText= "Return to Card List"
+        } else if (this.state.formVisibleOnPage) {
             currentlyVisibleState = <NewCardForm onNewCardCreation={this.handleAddingNewCardToList}/>
             buttonText = "Return to Card List"
         } else {
-            // passing mainCardList state as a prop to cardList <CardList cardList={this.state.mainCardList}/>
-            currentlyVisibleState = <CardList cardList={this.state.mainCardList}/>
+            // passing mainCardList and cardSelection state as a prop to cardList <CardList cardList={this.state.mainCardList}/>
+            currentlyVisibleState = <CardList cardList={this.state.mainCardList} onCardSelection={this.handleChangingSelectedCard}/>
             buttonText = "Link New Card"
         }
         return (
